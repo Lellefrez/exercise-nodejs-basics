a481a2b865a0cef81b54a6114b0e13d1a18f0f90
Manuel Frezzato <manuel.frezzato@gmail.com>
2024-01-10 22:50:14 +0100

    feat | test con apiKey

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import path from "path";
 import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+const apiKey = process.env.VITE_API_KEY;
 
-fetch(`https://api.themoviedb.org/3/movie/popular`)
+
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key${apiKey}`)
     .then(response => response.json())
     .then(obj => {
         console.log(obj);
